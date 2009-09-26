bc902953acda3400fd206f1ac80fd9b38ed70b65
John Schember <john@nachtimwald.com>
2009-09-26 12:29:59 -0400

    Remame italian translation.

---
+++ i18n/kdocker_it_IT.ts
@@ -0,0 +1,335 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it_IT">
+<context>
+    <name>KDocker</name>
+    <message>
+        <location filename="../src/kdocker.cpp" line="200"/>
+        <source>Usage: %1 [options] [command] -- [command options]</source>
+        <translation>Uso: %1 [opzioni] [comando] -- [opzioni comando]</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="201"/>
+        <source>Docks any application into the system tray</source>
+        <translation>Aggancia al vassoio di sistema qualsiasi applicazione</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="203"/>
+        <source>Command</source>
+        <translation>Comando</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="204"/>
+        <source>Run command and dock window</source>
+        <translation>Esegui comando e aggancia la finestra</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="205"/>
+        <source>Use -- after command to specify options for command</source>
+        <translation>Usa -- dopo il comando per specificare opzioni aggiuntive</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="207"/>
+        <source>Options</source>
+        <translation>Opzioni</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="208"/>
+        <source>Show author information</source>
+        <translation>Mostra informazioni sull&apos;autore</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="209"/>
+        <source>Don&apos;t warn about non-normal windows (blind mode)</source>
+        <translation>Non avvisare in caso di finestre anomale (modalità cieca)</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="210"/>
+        <source>Remove the window border</source>
+        <translation>Rimuovi il bordo della finestra</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="211"/>
+        <source>Maximum time in seconds to allow for command to start and open a window (defaults to 5 sec)</source>
+        <translation>Tempo massimo di esecuzione per permettere al comando di aprire una finestra (default 5 secondi)</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="212"/>
+        <source>Dock window that has focus (active window)</source>
+        <translation>Aggancia la finestra che ha focus (finestra attiva)</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="213"/>
+        <source>Display this help</source>
+        <translation>Mostra questo aiuto</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="215"/>
+        <source>Keep application window showing (dont hide on dock)</source>
+        <translation>Mantieni in primo piano la finestra (non nascondere quando agganciata)</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="216"/>
+        <source>Name used for matching when running command (fall back in case command is a launcher so pid and command won&apos;t match the window tile or class)</source>
+        <translation>Nome usato da associare quando si esegue il comando (fall back nel caso in cui il comando sia un launcher quindi pid e comando non corrispondano alla classe o tessera della finestra)</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="217"/>
+        <source>Iconify when obscured</source>
+        <translation>Riduci ad incona quando oscurato</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="218"/>
+        <source>Set ballooning timeout (popup time)</source>
+        <translation>Imposta durata fumetto (durata popup)</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="219"/>
+        <source>Disable ballooning title changes (quiet)</source>
+        <translation>Disabilita modifiche al titolo del fumetto (silenzioso)</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="220"/>
+        <source>Remove this application from the pager</source>
+        <translation>Rimuovi dal pager questa applicazione</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="221"/>
+        <source>Make the window sticky (appears on all desktops)</source>
+        <translation>Rendi fissa questa finestra (apparirà su tutti i desktops)</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="222"/>
+        <source>Remove this application from the taskbar</source>
+        <translation>Rimuovi dalla taskbar questa applicazione</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="223"/>
+        <source>Display version</source>
+        <translation>Mostra versione</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="224"/>
+        <source>Window id of the application to dock</source>
+        <translation>Id finestra dell&apos;applicazione da agganciare</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="225"/>
+        <source>Process id of the application to dock</source>
+        <translation>Id processo dell&apos;applicazione da agganciare</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="226"/>
+        <source>Force matching of command by using name instead of pid</source>
+        <translation>Forza il comando ad utlizzare il nome anzichè il pid</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="228"/>
+        <source>Bugs and wishes to https://bugs.launchpad.net/kdocker</source>
+        <translation>Bugs e suggerimenti a https://bugs.launchpad.net/kdocker</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="229"/>
+        <source>Project information at https://launchpad.net/kdocker</source>
+        <translation>Informazioni sul progetto disponibili all&apos;indirizzo https://bugs.launchpad.net/kdocker</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="234"/>
+        <source>Usage: %1 [options] command</source>
+        <translation>Uso: %1 [opzioni] comando</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="235"/>
+        <source>Try `%1 -h&apos; for more information</source>
+        <translation>Prova `%1 -h&apos; per aver maggiorni informazioni</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="240"/>
+        <source>%1 version: %2</source>
+        <translation>%1 versione %2</translation>
+    </message>
+    <message>
+        <location filename="../src/kdocker.cpp" line="241"/>
+        <source>Using Qt version: %1</source>
+        <translation>Versione Qt in uso: %1</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/constants.h" line="35"/>
+        <source>KDocker will help you dock any application into the system tray. This means you can dock openoffice, xmms, firefox, thunderbird, anything! Just point and click. Works for all NET WM compliant window managers - that includes KDE, GNOME, Xfce, Fluxbox and many more.
+
+Created by %1. Updated and maintained by %2.</source>
+        <translation>KDocker ti permetterà di ridurre ad icona nel vassoio di sistema qualsiasi applicazione. Questo significa che potrai ridurre ad icona openoffice, xmms, firefox, thunderbird, qualsiasi cosa! Semplicemente puntando e cliccando. Funziona con tutti i window managers conformi a NET WM come KDE, GNOME, Xfce, Fluxbox e molti altri ancora.
+
+Creato da %1. Aggiornato e mantenuto da: %2.</translation>
+    </message>
+</context>
+<context>
+    <name>Scanner</name>
+    <message>
+        <location filename="../src/scanner.cpp" line="63"/>
+        <location filename="../src/scanner.cpp" line="92"/>
+        <source>KDocker</source>
+        <translation>KDocker</translation>
+    </message>
+    <message>
+        <location filename="../src/scanner.cpp" line="63"/>
+        <source>%1 did not start properly.</source>
+        <translation>%1 non è stato avviato correttamente.</translation>
+    </message>
+    <message>
+        <location filename="../src/scanner.cpp" line="92"/>
+        <source>Could not find a matching window for %1 in the specified time: %2 seconds.</source>
+        <translation>Impossibile trovare una finestra corrispondente per %1 nel lasso di tempo specificato: %2 secondi.</translation>
+    </message>
+</context>
+<context>
+    <name>TrayItem</name>
+    <message>
+        <location filename="../src/trayitem.cpp" line="276"/>
+        <source>Select Icon</source>
+        <translation>Seleziona Icona</translation>
+    </message>
+    <message>
+        <location filename="../src/trayitem.cpp" line="359"/>
+        <source>About %1 - %2</source>
+        <translation>A proposito di %1 - %2</translation>
+    </message>
+    <message>
+        <location filename="../src/trayitem.cpp" line="361"/>
+        <source>See %1 for more information.</source>
+        <translation>Consulta %1 per maggiori informazioni.</translation>
+    </message>
+    <message>
+        <location filename="../src/trayitem.cpp" line="492"/>
+        <source>Show %1</source>
+        <translation>Mostra %1</translation>
+    </message>
+    <message>
+        <location filename="../src/trayitem.cpp" line="495"/>
+        <source>Hide %1</source>
+        <translation>Nascondi %1</translation>
+    </message>
+    <message>
+        <location filename="../src/trayitem.cpp" line="505"/>
+        <source>About %1</source>
+        <translation>A proposito di %1</translation>
+    </message>
+    <message>
+        <location filename="../src/trayitem.cpp" line="509"/>
+        <source>Options</source>
+        <translation>Opzioni</translation>
+    </message>
+    <message>
+        <location filename="../src/trayitem.cpp" line="510"/>
+        <source>Set icon</source>
+        <translation>Imposta icona</translation>
+    </message>
+    <message>
+        <location filename="../src/trayitem.cpp" line="513"/>
+        <source>Skip taskbar</source>
+        <translation>Salta taskbar</translation>
+    </message>
+    <message>
+        <location filename="../src/trayitem.cpp" line="518"/>
+        <source>Skip pager</source>
+        <translation>Salta pager</translation>
+    </message>
+    <message>
+        <location filename="../src/trayitem.cpp" line="523"/>
+        <source>Sticky</source>
+        <translation>Fissa</translation>
+    </message>
+    <message>
+        <location filename="../src/trayitem.cpp" line="528"/>
+        <source>Iconify when minimized</source>
+        <translation>Riduci ad icona quando minimizzata</translation>
+    </message>
+    <message>
+        <location filename="../src/trayitem.cpp" line="533"/>
+        <source>Iconify when obscured</source>
+        <translation>Riduci ad icona quando oscurata</translation>
+    </message>
+    <message>
+        <location filename="../src/trayitem.cpp" line="538"/>
+        <source>Iconify when focus lost</source>
+        <translation>Riduci ad icona quando perde focus</translation>
+    </message>
+    <message>
+        <location filename="../src/trayitem.cpp" line="543"/>
+        <source>Balloon title changes</source>
+        <translation>Il titolo del fumetto cambia</translation>
+    </message>
+    <message>
+        <location filename="../src/trayitem.cpp" line="550"/>
+        <source>Dock Another</source>
+        <translation>Aggancia un&apos;altra</translation>
+    </message>
+    <message>
+        <location filename="../src/trayitem.cpp" line="551"/>
+        <source>Undock All</source>
+        <translation>Sgancia tutti</translation>
+    </message>
+    <message>
+        <location filename="../src/trayitem.cpp" line="553"/>
+        <source>Toggle</source>
+        <translation>Commuta</translation>
+    </message>
+    <message>
+        <location filename="../src/trayitem.cpp" line="556"/>
+        <source>Undock</source>
+        <translation>Sgancia</translation>
+    </message>
+    <message>
+        <location filename="../src/trayitem.cpp" line="557"/>
+        <source>Close</source>
+        <translation>Chiudi</translation>
+    </message>
+</context>
+<context>
+    <name>TrayItemManager</name>
+    <message>
+        <location filename="../src/trayitemmanager.cpp" line="154"/>
+        <location filename="../src/trayitemmanager.cpp" line="195"/>
+        <location filename="../src/trayitemmanager.cpp" line="247"/>
+        <location filename="../src/trayitemmanager.cpp" line="289"/>
+        <location filename="../src/trayitemmanager.cpp" line="297"/>
+        <source>KDocker</source>
+        <translation>KDocker</translation>
+    </message>
+    <message>
+        <location filename="../src/trayitemmanager.cpp" line="154"/>
+        <source>Cannot dock the active window because no window has focus.</source>
+        <translation>Impossibile agganciare la finestra attiva perchè priva di focus.</translation>
+    </message>
+    <message>
+        <location filename="../src/trayitemmanager.cpp" line="195"/>
+        <source>Invalid window id.</source>
+        <translation>Id finestra invalido.</translation>
+    </message>
+    <message>
+        <location filename="../src/trayitemmanager.cpp" line="247"/>
+        <source>This window is already docked.
+Click on system tray icon to toggle docking.</source>
+        <translation>Questa finestra è già agganciata.
+Clicca sull&apos;icona corrispondente nel vassoio di sistema per commutare.</translation>
+    </message>
+    <message>
+        <location filename="../src/trayitemmanager.cpp" line="282"/>
+        <source>Select the application/window to dock with the left mouse button.</source>
+        <translation>Seleziona l&apos;applicazione/finestra da agganciare cliccando con il tasto sinistro del mouse.</translation>
+    </message>
+    <message>
+        <location filename="../src/trayitemmanager.cpp" line="283"/>
+        <source>Click any other mouse button to abort.</source>
+        <translation>Click con un qualsiasi tasto del mouse per annullare.</translation>
+    </message>
+    <message>
+        <location filename="../src/trayitemmanager.cpp" line="297"/>
+        <source>The window you are attempting to dock does not seem to be a normal window.</source>
+        <translation>La finestra che stai cercando di agganciare sembra non essere una normale finestra.</translation>
+    </message>
+</context>
+</TS>
