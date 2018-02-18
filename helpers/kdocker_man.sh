ca0664bb8a43bcdc330bd47ff122a04d3f1af999
John Schember <john@nachtimwald.com>
2018-02-18 10:06:01 -0500

    Update changelog for 5.1 release.

--- helpers/kdocker_man.sh
+++ helpers/kdocker_man.sh
@@ -3,6 +3,6 @@
 # Create man page (kdocker.1) from pod template
 
 pod2man --center  'General Commands Manual' \
-        --release 'Version 5.0'             \
+        --release 'Version 5.1'             \
         --date    "$(date +'%e %B, %Y')"    \
         kdocker.pod  kdocker.1
