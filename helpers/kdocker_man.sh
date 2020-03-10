a067154d77693718a7b04aa2aa03e19ce0ae1cb3
John Schember <john@nachtimwald.com>
2020-03-10 10:04:31 -0400

    Bump version.

--- helpers/kdocker_man.sh
+++ helpers/kdocker_man.sh
@@ -3,6 +3,6 @@
 # Create man page (kdocker.1) from pod template
 
 pod2man --center  'General Commands Manual' \
-        --release 'Version 5.2'             \
+        --release 'Version 5.3'             \
         --date    "$(date +'%e %B, %Y')"    \
         kdocker.pod  kdocker.1
--- src/constants.cpp
+++ src/constants.cpp
@@ -24,7 +24,7 @@ const QString Constants::APP_NAME    = "KDocker";
 const QString Constants::ORG_NAME    = "com.kdocker";
 const QString Constants::DOM_NAME    = "kdocker.com";
 const QString Constants::WEBSITE     = "https://github.com/user-none/KDocker";
-const QString Constants::APP_VERSION = "5.2";
+const QString Constants::APP_VERSION = "5.3";
 
 const char *Constants::OPTIONSTRING = "+abd:e:fhi:I:jklmn:op:qrstvuw:x:";
 
