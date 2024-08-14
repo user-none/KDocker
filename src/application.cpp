5dec3dfe825fefdea5872693307ba7db2dd26a50
John Schember <john@nachtimwald.com>
2024-08-13 20:02:09 -0400

    silence unused result warnings when using socket to signal.

--- src/application.cpp
+++ src/application.cpp
@@ -40,13 +40,13 @@ void Application::setKDockerInstance(KDocker *kdocker) {
 
 void Application::notifyCloseSignal() {
     char tmp = 1;
-    ::write(m_closeSignalFd[0], &tmp, sizeof(tmp));
+    static_cast<void>(::write(m_closeSignalFd[0], &tmp, sizeof(tmp)));
 }
 
 void Application::handleCloseSignal() {
     m_closeSignalSocketNotifier->setEnabled(false);
     char tmp;
-    ::read(m_closeSignalFd[1], &tmp, sizeof(tmp));
+    static_cast<void>(::read(m_closeSignalFd[1], &tmp, sizeof(tmp)));
 
     if (m_kdocker) {
         m_kdocker->undockAll();
